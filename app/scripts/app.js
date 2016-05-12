3d4903ff33e7384067118ccb7271e0fa27020eae
Your Name <you@example.com>
2016-05-12 07:16:31 -0700

    stylesheets-n-landingCtrl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,19 +5,20 @@
 
  (function() {
      function config($stateProvider, $locationProvider) {
-           $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });   
-         
-           $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
-                        })
-       
-     }
+                                                           $locationProvider
+                                                           .html5Mode({
+                                                                         enabled: true,
+                                                                         requireBase: false
+                                                                     });   
+
+                                                             $stateProvider
+                                                             .state('landing', {
+                                                                                 url: '/',
+                                                                                 controller: 'LandingCtrl as landing',
+                                                                                 templateUrl: '/templates/landing.html'
+                                                                                })
+
+                                                     }
  
 
      angular
---
+++ dist/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,13 @@
+///*
+(function() {
+    function LandingCtrl() {
+        this.test = "Hello world";
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('LandingCtrl', LandingCtrl);
+})();
+
+
+//*/
